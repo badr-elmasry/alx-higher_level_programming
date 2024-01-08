021b3b5c975a79d6583a3ff424d9a62e533fd6b8
badr-elmasry <badr.ck.elmasry@gmail.com>
2024-01-08 10:58:02 -0800

    push

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for x in my_list:
-        print("{:d}".format(x))
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+    for num in my_list:
+        print("{:d}".format(num))
