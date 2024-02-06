aeee18b1507551aec549545595232f7ed2513b51
badr-elmasry <badr.ck.elmasry@gmail.com>
2024-02-06 15:39:55 -0800

    python

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""write_file module.
+
+Contains a function that writes a text file.
+"""
 
 
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
